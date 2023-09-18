ac47f79aa86fce8e6824d27cea3b72607ea9bb38
Ethan Grunewald <grunet01@luther.edu>
2023-09-17 20:17:25 -0500

    Completed Calculator Project

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -5,25 +5,42 @@
 var outputScreen;
 var clearOnEntry;
 
+const maxDisplayCharacters = 26;
 
 /**
  * Display a digit on the `outputScreen`
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    if (outputScreen.textContent.length < maxDisplayCharacters) {
+        if (outputScreen.textContent == "0") {
+            outputScreen.textContent = String(digit);
+        } else {
+            outputScreen.textContent += digit;
+        }
+    }
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    outputScreen.textContent = "0";
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    try {
+        outputScreen.textContent = eval(outputScreen.textContent);
+    } catch (e) {
+        outputScreen.textContent = "ERROR";
+    }
+}
 
 
 /**
@@ -31,10 +48,14 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    if (outputScreen.textContent != "0" && outputScreen.textContent.length < maxDisplayCharacters) {
+        outputScreen.textContent += operation;
+    }
+}
 
 
 window.onload = function () {
-    outputScreen = document.querySelector("#result");
+    outputScreen = document.querySelector('#result');
     clearOnEntry = true;
 };
