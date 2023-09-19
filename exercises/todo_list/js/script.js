b7bfe2e633204dc4f6556fc690fef2fa135ea1c3
Ethan Grunewald <grunet01@luther.edu>
2023-09-18 22:00:22 -0500

    First iteration of TODO List exercise

--- exercises/todo_list/js/script.js
+++ exercises/todo_list/js/script.js
@@ -11,43 +11,61 @@ var priority = ["Low", "Normal", "Important", "Critical"];
  * Validate form, collect input values, and add call `addRow` to add a new row to the table
  */
 function addTask() {
-    // TODO: Implement this function
-    let vals = [];
     let rowcolids = ["title", "assignedTo", "priority", "dueDate"];
+    let rowData = {};
 
-    addRow(vals, document.getElementById("taskList"));
+    rowcolids.forEach(id => rowData[id] = document.getElementById(id).value );
+
+    if (Object.values(rowData).includes("")) {
+        document.getElementById("feedbackMessage").hidden = false;
+    } else {
+        document.getElementById("feedbackMessage").hidden = true;
+        addRow(rowData, document.getElementById("taskList"));
+    }
 }
 
 /**
  * Add a new row to the table
  * 
- * @param {string[]} valueList list of task attributes
- * @param {Object} parent DOM node to append to
+ * @param {Object} rowData list of task attributes
+ * @param {Node} parent DOM node to append to
  */
-function addRow(valueList, parent) {
-    // TODO: Implement this function
+function addRow(rowData, parent) {
     let row = document.createElement("tr");
-    let td = document.createElement("td");
-    let cb = document.createElement("input");
+    row.className = rowData.priority.toLowerCase();
 
-    parent.appendChild(row);
+    row.innerHTML = `<th scope="row" class="text-center">
+                        <input type="checkbox" class="form-check-input" onchange="removeRow(this)"/>
+                    </th>
+                    <td>${rowData.title}</td>
+                    <td>${rowData.assignedTo}</td>
+                    <td>${rowData.priority}</td>
+                    <td>${rowData.dueDate}</td>`
+
+    parent.querySelector('tbody').appendChild(row);
 }
 
 /**
  * Remove a table row corresponding to the selected checkbox
  * 
+ *  @param {Node} caller DOM node calling removeRow
  * https://stackoverflow.com/questions/26512386/remove-current-row-tr-when-checkbox-is-checked
  */
-function removeRow() {
-    // TODO: Implement this function
+async function removeRow(caller) {
+    await new Promise(r => setTimeout(r, 1000));
+    caller.closest("tbody").removeChild(caller.closest("tr"));
 }
 
 /**
  * Remove all table rows
  * 
  */
-function selectAll() {
-
+async function selectAll() {
+    checkboxes = document.getElementsByClassName("form-check-input");
+    for (const checkbox of checkboxes) {
+        checkbox.checked = true;
+        removeRow(checkbox);
+    }
 }
 
 /**
@@ -57,8 +75,12 @@ function selectAll() {
  * @param {string[]} sList array of options
  */
 function populateSelect(selectId, sList) {
-    // TODO: Implement this function
     let sel = document.getElementById(selectId, sList);
+    for (const s of sList) {
+        let option = document.createElement("option");
+        option.text = s;
+        sel.add(option);
+    }
 }
 
 window.onload = function () {
