32fd5d83fbea452195744fbe9500df1e23d2effa
Ethan's VM <grunet01@luther.edu>
2023-10-30 23:53:54 -0500

    Completed jokes exercise

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -5,25 +5,73 @@ import random
 from typing import List
 
 import pyjokes
-from flask import Flask, render_template, request
+from flask import Flask, make_response, redirect, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    """Render the template with form"""
-    raise NotImplementedError
+    return render_template("base.html")
 
 
 @app.route("/", methods=["POST"])
 def index_jokes():
-    """Render the template with jokes"""
-    raise NotImplementedError
+
+    language = request.form.get("language")
+    category = request.form.get("category")
+    number = request.form.get("number")
+
+    if (number != None):
+        number = int(number)
+
+    jokes = send_joke(
+        language=language, 
+        category=category, 
+        number=number
+        )
+
+    response = make_response(render_template(
+            "jokes.html", 
+            jokes=jokes
+            ))
+    
+    return response
+
+    
 
 
 def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
-    """Return a list of jokes"""
-    raise NotImplementedError
+
+    if (language == None):
+        return send_joke(category=category, number=number)
+    
+    if (category == None):
+        return send_joke(language=language, number=number)
+    
+    if (number == None):
+        return send_joke(language=language, category=category)
+    
+
+    try:
+        allJokes = pyjokes.get_jokes(
+            language=language,
+            category=category)
+
+    except pyjokes.pyjokes.CategoryNotFoundError:
+        return ["No kidding!"]
+    
+    
+    if (number >= len(allJokes)):
+        return allJokes
+    
+
+    jokes = []
+
+    for _ in range(number):
+        jokes.append(allJokes[random.randint(0, len(allJokes) - 1)])
+    
+    
+    return jokes
