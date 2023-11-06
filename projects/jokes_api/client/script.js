8ac538608c6383c13f9e645341a5e69ac879b71b
Ethan Grunewald <grunet01@luther.edu>
2023-11-06 11:07:50 -0600

    Joke selection working

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -1,2 +1,46 @@
 'use strict';
 
+const APILocation = 'https://jokes-api-7apx.onrender.com/api/v1/jokes?';
+
+async function getJokes() {
+
+    var language = document.querySelector('#selLang').value;
+    var category = document.querySelector('#selCat').value;
+    var number = document.querySelector('#selNum').value;
+
+    var response = await fetch(APILocation + new URLSearchParams({
+        language: language,
+        category: category,
+        number: number
+    }))
+    .then(response => response.json())
+    .catch(error => displayIdError());
+
+    
+    var jokesDiv = document.querySelector('#jokes');
+    jokesDiv.textContent = '';
+
+    for (var joke of response) {
+
+        var jokeNode = document.createElement('li');
+        jokeNode.textContent = joke;
+        
+        jokesDiv.appendChild(jokeNode);
+
+    }
+
+}
+
+
+function displayIdError() {
+
+    console.log("We fucked up");
+
+}
+
+
+
+document.querySelector('#btnAmuse').addEventListener(
+    'click', 
+    (event) => getJokes()
+);
